3ea73f3b25256dc7bf61c5be098b2ec8de113370
michaelauditor-pixel <michael.auditor@gmail.com>
2026-03-10 23:56:00 -0300

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+    <meta name="google-site-verification" content="TOKEN_AQUI" />
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
